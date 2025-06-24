1683e86230836b044ab197f9deb7ac6cab5052ff
ERIK ELIAS VARELA DE SOUSA <a95415195@rn.senac.local>
2025-06-24 09:14:28 -0300

    feat: questão 09 - null e undefined TypeScript

---
+++ questao09/index.ts
@@ -0,0 +1,8 @@
+// Questão 09. Escreva um programa TypeScript que declare uma variável 'eNulo' e atribua a ela null. Você também deve declarar uma variável chamada 'eIndefinida' e atribuir a ela um valor undefined.
+
+let eNulo: null = null;
+
+let eIndefinida: undefined = undefined;
+
+console.log(`O valor de eNulo é: ${eNulo}`);
+console.log(`O valor de eIndefinida é: ${eIndefinida}`);
