434042fca1620e114a8362009e8798e1b9051999
ERIK ELIAS VARELA DE SOUSA <a95415195@rn.senac.local>
2025-06-24 08:45:59 -0300

    feat: questão 06 - conversões de tipo + descrições nas questões

---
+++ questao05/index.ts
@@ -0,0 +1,7 @@
+// Ele declara o tipo da variável como string de forma automática
+let nome = "Erik";
+
+nome = "Thiago"; // Fazendo a atribuição de um valor de tipo string seja permitido
+nome = 10; // Fazendo a atribuição de um valor de tipo number não seja permitido, pois o tipo da variável é string
+
+export {};
---
+++ questao06/index.ts
@@ -0,0 +1,7 @@
+let String: string = "40";
+console.log(String + 10);
+
+let Number = parseInt(String);
+console.log(Number + 10);
+
+export {};
